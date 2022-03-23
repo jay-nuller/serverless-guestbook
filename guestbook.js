3bff22a7984448d4c56b5aa8e8e45db2170727ea
Jay Nuller <96411018+jay-nuller@users.noreply.github.com>
2022-03-23 00:34:38 -0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7f4c681b.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
